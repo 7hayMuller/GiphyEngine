e775ca31b4af3dfe20a36ed07d62cd95a8fa875f
7hayMuller <thaynamuller88@gmail.com>
2021-04-13 18:17:16 -0300

    Adding GridView component and implements request to get gifs from API

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,16 @@ import 'package:giphy_io/ui/home_page.dart';
 void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.pinkAccent,
+        primaryColor: Colors.pinkAccent,
+        cursorColor: Colors.pinkAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          enabledBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.pinkAccent)),
+          focusedBorder:
+              OutlineInputBorder(borderSide: BorderSide(color: Colors.pinkAccent)),
+          hintStyle: TextStyle(color: Colors.white),
+        )),
   ));
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -8,7 +8,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   int _offset = 0;
 
   String _search;
@@ -35,9 +34,77 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.all(10.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Search for gifs here',
+                    labelStyle: TextStyle(color: Colors.white),
+                    border: OutlineInputBorder()),
+                style: TextStyle(color: Colors.white, fontSize: 15.0),
+                textAlign: TextAlign.center,
+                onSubmitted: (text) {
+                  setState(() {
+                    _search = text;
+                  });
+                },
+              )),
+          Expanded(
+              child: FutureBuilder(
+            future: _getGifs(),
+            builder: (context, snapshot) {
+              switch (snapshot.connectionState) {
+                case ConnectionState.waiting:
+                case ConnectionState.none:
+                  return Container(
+                    width: 200.00,
+                    height: 200.00,
+                    alignment: Alignment.center,
+                    child: CircularProgressIndicator(
+                      valueColor:
+                          AlwaysStoppedAnimation<Color>(Colors.greenAccent),
+                      strokeWidth: 5.0,
+                    ),
+                  );
+                default:
+                  if (snapshot.hasError)
+                    return Container();
+                  else
+                    return _createGifTable(context, snapshot);
+              }
+            },
+          ))
+        ],
+      ),
+    );
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: snapshot.data['data'].length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            child: Image.network(
+              snapshot.data['data'][index]['images']['fixed_height']['url'],
+              height: 300.0,
+              fit: BoxFit.cover,
+            ),
+          );
+        });
   }
 }
